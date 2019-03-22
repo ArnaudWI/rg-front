e8565182acccce515a24401fa268aa1a0af6ffac
Arnaud Wissler <arnaud.wissler@gmail.com>
2019-03-22 13:34:03 +0100

    intégration pages de connexion effectué

--- Components/Screens/Connexion/HomeScreen.js
+++ Components/Screens/Connexion/HomeScreen.js
@@ -1,29 +1,77 @@
 import React from 'react';
 //import bibliothèque react native
-
+import {
+  ImageBackground,
+  StyleSheet,
+  Image
+} from 'react-native';
 //import bibliothèque native base
 import {
   View,
   Button,
-  Text,
-  H3
+  Text
 } from 'native-base';
 
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{flex:1,  alignSelf: 'center', justifyContent: 'center'}}>
-          <H3>HomeScreen</H3>
-          <Button danger onPress={ ()=> this.props.navigation.navigate('SignUp')}>
-            <Text>Sign Up</Text>
+
+      <ImageBackground style={{flex:1}} source={require("../../../public/images/img-bg.jpg")}>
+      <View style={styles.view}>
+
+      <Image source={require('../../../public/logo/logo-rg.png')} style={styles.logo}/>
+
+          <Button style={styles.bouton1} onPress={ ()=> this.props.navigation.navigate('SignUp')}>
+            <Text style={styles.textBouton} >Se connecter</Text>
           </Button>
 
-          <Button danger onPress={ ()=> this.props.navigation.navigate('SignIn')}>
-            <Text>Sign In</Text>
+          <Button style={styles.bouton2} onPress={ ()=> this.props.navigation.navigate('SignIn')}>
+            <Text style={styles.textBouton} >S'inscrire</Text>
           </Button>
 
       </View>
+      </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignSelf: 'center',
+  },
+  logo: {
+    height: 225,
+    width: 225,
+    marginTop: 50
+  },
+  bouton2: {
+    height: 50,
+    width: 170,
+    backgroundColor: '#E52D2F',
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 0,
+    alignSelf: 'center',
+    marginTop: 30
+  },
+  textBouton: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+  bouton1: {
+    height: 50,
+    width: 170,
+    backgroundColor: '#E52D2F',
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 0,
+    alignSelf: 'center',
+    marginTop: 60
+  }
+})
--- Components/Screens/Connexion/SignInScreen.js
+++ Components/Screens/Connexion/SignInScreen.js
@@ -1,26 +1,104 @@
 import React from 'react';
 //import bibliothèque react native
-
+import {
+  ImageBackground,
+  StyleSheet,
+  Image
+} from 'react-native';
 //import bibliothèque native base
 import {
   View,
   Button,
   Text,
-  H3
+  Form,
+  Item,
+  Input,
+  Label
 } from 'native-base';
 
 
-export default class HomeScreen extends React.Component {
+export default class SignInScreen extends React.Component {
   render() {
     return (
-      <View style={{flex:1,  alignSelf: 'center', justifyContent: 'center'}}>
-          <H3>Sign In</H3>
-          <Button danger onPress={ ()=> this.props.navigation.navigate('Accueil')}>
-            <Text>Accueil</Text>
+      <ImageBackground style={{flex:1}} source={require("../../../public/images/img-bg.jpg")}>
+      <View style={styles.view}>
+
+      <Image source={require('../../../public/logo/logo-rg.png')} style={styles.logo}/>
+
+          <Form style={styles.form}>
+            <Item floatingLabel style={styles.item}>
+              <Label style={styles.label} >Prénom</Label>
+              <Input style={styles.input}/>
+            </Item>
+            <Item floatingLabel style={styles.item}>
+              <Label style={styles.label} >Nom</Label>
+              <Input style={styles.input}/>
+            </Item>
+            <Item floatingLabel style={styles.item}>
+              <Label style={styles.label} >Email</Label>
+              <Input style={styles.input}/>
+            </Item>
+            <Item floatingLabel style={styles.item}>
+              <Label style={styles.label} >Mot de passe</Label>
+              <Input style={styles.input}/>
+            </Item>
+          </Form>
+
+          <Button style={styles.bouton} onPress={ ()=> this.props.navigation.navigate('Accueil')}>
+            <Text style={styles.textBouton}>Valider mon inscription</Text>
           </Button>
 
-
       </View>
+      </ImageBackground>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignSelf: 'center',
+  },
+  logo: {
+    height: 225,
+    width: 225,
+    marginTop: 50,
+    alignSelf: 'center'
+  },
+  textBouton: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+  bouton: {
+    height: 50,
+    width: 270,
+    backgroundColor: '#E52D2F',
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 0,
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  form: {
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    paddingRight: 20,
+    paddingBottom: 20,
+    marginTop: 10,
+    width: 300
+  },
+  item: {
+    borderColor: '#E52D2F'
+  },
+  label: {
+    color: '#E52D2F',
+    fontWeight: 'bold'
+  },
+  input: {
+    color: '#E52D2F',
+    fontWeight: 'bold'
+  }
+})
